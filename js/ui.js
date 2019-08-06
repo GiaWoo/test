9f2ef471bb4f175189575f34eac956433bca5934
GiaWoo <42565907+GiaWoo@users.noreply.github.com>
2019-08-06 16:36:34 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -63,10 +63,5 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         }
     }
 });
-$("#carouselExample").swiperight(function() {
-    $(this).carousel('prev');
-});
-$("#carouselExample").swipeleft(function() {
-    $(this).carousel('next');
-});
+
   
