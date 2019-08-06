a0395d9c8c7e6ca5919765c3232c00bbfe2a54a5
GiaWoo <42565907+GiaWoo@users.noreply.github.com>
2019-08-06 17:38:40 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -42,43 +42,23 @@ $(function() {
         });
         return idpanel;
     };
-});
-
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 50,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
             }
         }
-    }
+    });
 });
 
-
-$('#carouselExample').carousel({ 
-   interval: 2000
-});
-
-
-$(document).ready(function() {
-   $('a.thumb').click(function(event){
-      event.preventDefault();
-      var content = $('.modal-body');
-      content.empty();
-      var title = $(this).attr("title");
-      $('.modal-title').html(title);        
-      content.html($(this).html());
-      $(".modal-profile").modal({show:true});
-   });
-});
   
