125b760b94521e2f4440afcce8865005ce57577a
GiaWoo <42565907+GiaWoo@users.noreply.github.com>
2019-08-08 14:59:03 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -69,6 +69,10 @@ $(function() {
             }
         }
     });
+    var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     function onYouTubeIframeAPIReady() {
 	  var player;
 	  player = new YT.Player('player', {
