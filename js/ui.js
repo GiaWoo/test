8801d1631f1e562d2f1bec9d3d9cf415c6f8bc38
GiaWoo <42565907+GiaWoo@users.noreply.github.com>
2019-08-08 14:47:50 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -69,6 +69,31 @@ $(function() {
             }
         }
     });
+    var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '100%',
+          width: '100%',
+          videoId: '7rJmj4unFIc',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+      var done = false;
+      function stopVideo() {
+        player.stopVideo();
+      }
 });
 
   
